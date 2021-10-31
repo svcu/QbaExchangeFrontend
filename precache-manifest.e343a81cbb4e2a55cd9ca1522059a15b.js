3ae7358c2c88b3746a90ad9e8eaa08a968bd5930
Sergio Valdés Valdés <60680494+svcu@users.noreply.github.com>
2021-10-30 20:23:36 -0400

    Add files via upload

---
+++ precache-manifest.e343a81cbb4e2a55cd9ca1522059a15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea0405179622b0220d804bb03902937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3db10a6cf661f94c92e5",
+    "url": "/static/css/main.d98a1b8f.chunk.css"
+  },
+  {
+    "revision": "398761f62178b50908e6",
+    "url": "/static/js/2.b25034f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b25034f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db10a6cf661f94c92e5",
+    "url": "/static/js/main.b968096b.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  }
+]);
